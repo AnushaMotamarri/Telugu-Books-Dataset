5fa25bceda52c9beedfa5645791a24161d9a61d8
AnushaMotamarri <motamarrianusha01@gmail.com>
2018-09-21 18:25:31 +0530

    pages of books

---
+++ extract_linksofpages.py
@@ -0,0 +1,20 @@
+import requests
+import os,sys
+from bs4 import BeautifulSoup
+def pages():
+    i=1
+    for book in open("booklinks.txt",'r',encoding="utf-8"):
+        if(i>=96 and i<=195):
+            file="book"+str(i)+".txt"
+            print(i)
+            f1=open(file,'w+',encoding="utf-8")
+            resp=requests.get(book.strip())
+            if resp.status_code==200:
+                soup=BeautifulSoup(resp.text,'html.parser')
+                select=soup.findAll("select")
+                for option in select[0].findAll("option"):
+                    link=option.get("value")
+                    f1.write(link+"\n")
+
+        i=i+1
+pages()
