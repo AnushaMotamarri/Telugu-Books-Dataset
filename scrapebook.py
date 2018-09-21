96ccb8b04603be948b608d5c6f237a2c7ca53707
AnushaMotamarri <motamarrianusha01@gmail.com>
2018-09-21 18:29:38 +0530

    scrape

---
+++ scrapebook.py
@@ -0,0 +1,19 @@
+import requests
+import os,sys
+from bs4 import BeautifulSoup
+def scrape():
+    os.mkdir("book_data")
+    for i in range(1,2):
+        n_url=-1
+        for page in open("book"+str(i)+".txt",'r'):
+            url=page.strip()
+            if(url!=""):
+                n_url+=1
+                resp=requests.get(url)
+                if resp.status_code==200:
+                    soup=BeautifulSoup(resp.text,'html.parser')
+                    filename="00"+'0'*(3-len(str(i)))+str(i)+'0'*(3-len(str(n_url)))+str(n_url)+".utf8"
+                    f=open("book_data/"+filename,'a+',encoding='utf_8')
+                    for paras in soup.findAll("p"):
+                        f.write(str(paras.get_text()))
+scrape()
